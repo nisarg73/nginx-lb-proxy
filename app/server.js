8dd184fd11155e114dac5e16b5860f914f1f3812
Nisarg Patel <nisargmpatel73@gmail.com>
2020-09-04 03:22:12 +0530

    Make a simple node server and its docker image

---
+++ app/server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+
+const HOST = '0.0.0.0'
+const PORT = '3110'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(PORT, HOST, () => {
+  console.log(`Listening to http://${HOST}:${PORT}`)
+})
